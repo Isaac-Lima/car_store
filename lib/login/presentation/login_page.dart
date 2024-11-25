db8b0547c52704ac7e5657792d873024091cbfc4
Isaac Lima <isaac.lima25@aluno.ce.gov.br>
2024-11-25 16:21:38 -0300

    :sparkles: feat: Adicionado página de login

---
+++ lib/login/presentation/login_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF9FAFB),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 85),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  "assets/images/letter_logo.png",
+                ),
+              ),
+              SizedBox(height: 90),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                    const Text("Entrar"),
+                    const Text("Bem-Vindo a CarStore"),
+                    SizedBox(height: 30),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3))
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.person,
+                              size: 40,
+                            ),
+                            labelText: "Usuário",
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelStyle: TextStyle(color: Colors.grey),
+                            border:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12)),
+                        cursorColor: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3))
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.lock,
+                              size: 40,
+                            ),
+                            labelText: "Senha",
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelStyle: TextStyle(color: Colors.grey),
+                            border:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12)),
+                        cursorColor: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Esqueceu a senha ?",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFFF5C00),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: () {},
+                      child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: Center(
+                            child: Text("Entrar"),
+                          )),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
